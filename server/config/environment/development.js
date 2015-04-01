5810e8990f350a5815ee4ea77c5a7fc51d4cb04b
nikolai muhhin <test@test.ee>
2015-04-01 20:26:55 +0000

    Configure database for development environmentgit diff server/config/environment/development.js

--- server/config/environment/development.js
+++ server/config/environment/development.js
@@ -5,7 +5,7 @@
 module.exports = {
   // MongoDB connection options
   mongo: {
-    uri: 'mongodb://deals:offers@dogen.mongohq.com:10039/deals_development'
+    uri: 'mongodb://deals:offers@dogen.mongohq.com:10062/deals_public_development'
   },
 
   seedDB: true
