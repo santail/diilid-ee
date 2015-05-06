b9b6d088aee812b680e2b133b551bb2fb18739fc
nikolai muhhin <test@test.ee>
2015-05-06 11:09:23 +0000

    Override Angular resource for Wish adding POST save method

--- client/components/business/wish.service.js
+++ client/components/business/wish.service.js
@@ -4,5 +4,10 @@ angular.module('workspaceApp')
   .factory('Wish', function ($resource) {
     return $resource('/api/wishes/:id/:controller', {
       id: '@_id'
+    }, {
+      save: {
+        method: 'POST',
+        isArray: true
+      }
     });
   });
